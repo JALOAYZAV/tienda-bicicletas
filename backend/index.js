873d64d75c413568c069982f1828257824bc8f5c
Christian Cazorla <cazorladevnet@gmail.com>
2025-04-15 15:48:21 -0500

    Backend: Products Category

--- backend/index.js
+++ backend/index.js
@@ -18,13 +18,13 @@ const authRoutes = require('./routes/authRoutes');
 // const userRoutes = require('./routes/userRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 const productRoutes = require('./routes/productRoutes');
-// const categoryRoutes = require('./routes/categoryRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
 
 app.use('/api/auth', authRoutes);
 // app.use('/api/users', userRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/products', productRoutes);
-// app.use('/api/categories', categoryRoutes);
+app.use('/api/categories', categoryRoutes);
 
 // Manejo de errores centralizado
 app.use((err, req, res, next) => {
